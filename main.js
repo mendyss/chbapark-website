1e10717cec53a60386361c2231f0b634447b5023
mendyss <50183533+mendyss@users.noreply.github.com>
2025-02-10 01:10:19 +0200

    Update

---
+++ main.js
@@ -0,0 +1,7 @@
+// אם אינך מעוניין בקובץ JS נפרד, אפשר פשוט להשאיר ריק
+// או למחוק את <script src="main.js"></script> מה־index.html
+
+function toggleMenu() {
+  var nav = document.getElementById("main-nav");
+  nav.classList.toggle("hidden");
+}
