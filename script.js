d519d8fd1424680b9268da95edca9120a31c908c
mendyss <50183533+mendyss@users.noreply.github.com>
2025-02-10 23:16:11 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// אם אינך מעוניין בקובץ JS נפרד, אפשר פשוט להשאיר ריק
-// או למחוק את <script src="main.js"></script> מה־index.html
-
-function toggleMenu() {
-  var nav = document.getElementById("main-nav");
-  nav.classList.toggle("hidden");
-}
